c97ac29a8d6940f6699a503cb48c6d8f17072800
shhpark <shpark@yonsanzip.com>
2019-03-06 08:50:47 +0900

    사용자 인증

--- routes/web.php
+++ routes/web.php
@@ -58,4 +58,34 @@
 
 //Route::get('/', 'WelcomeController@index');
 
-Route::resource('articles', 'ArticlesController');
+//Route::resource('articles', 'ArticlesController');
+
+Route::get('/', 'WelcomeController@index');
+
+Route::get('auth/login', function (){
+    $credentials = [
+        'email' => 'john@example.com',
+        'password' => 'password'
+    ];
+
+    if (!auth()->attempt($credentials)){
+        return '로그인 정보가 정확하지 않습니다.';
+    }
+
+    return redirect('protected');
+});
+
+Route::get('protected',['middleware' => 'auth', function (){
+    dump(session()->all());
+
+    return '어서오세요' . auth()->user()->name;
+}]);
+
+Route::get('auth/logout',function () {
+    auth()->logout();
+
+    return '또 봐요~';
+});
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');

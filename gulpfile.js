5ddabe8f2a7139afc3ef90105ab800d58f094a3a
shhpark <shpark@yonsanzip.com>
2019-03-11 09:30:40 +0900

    엘릭서와 프런트엔드

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+const elixir = require('laravel-elixir');
+require('laravel-elixir-vue');
+
+elixir(mix => {
+    mix.sass('app.scss');
+    mix.webpack('app.js');
+    mix.scripts([
+        'node_modules/highlightjs/highlight.pack.js',
+        'public/js/app.js'
+    ],'public/js/app.js');
+    mix.version([
+       'css/app.css',
+       'js/app.js'
+    ]);
+    mix.copy('node_modules/font-awesome/fonts','public/build/fonts');
+    mix.browserSync({proxy:'localhost:8000'}); // 브라우저 싱크
+});
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,16 +10,18 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    {{--<script src="{{ asset('js/app.js') }}" defer></script>--}}
+    <script src="{{ elixir('js/app.js') }}"></script>
 
-    <!-- Fonts -->
+<!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    {{--<link href="{{ asset('css/app.css') }}" rel="stylesheet">--}}
+    <link href="{{ elixir('css/app.css') }}" rel="stylesheet">
 </head>
-<body>
+<body id="app-layout">
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
             <div class="container">
